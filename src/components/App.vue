894f6ca919fb8430d62729458c87c7b82eace06d
EGOIST <0x142857@gmail.com>
2021-10-24 21:42:00 +0800

    tweaks

--- src/components/App.vue
+++ src/components/App.vue
@@ -250,11 +250,10 @@ watchEffect(() => {
         </div>
 
         <div class="w-1/2 h-full overflow-auto">
-          <div
-            v-if="esbuildLoading"
-            class="p-3 bg-blue-500 rounded-lg text-white mb-4"
-          >
-            Loading the WASM build of esbuild..
+          <div class="p-3" v-if="esbuildLoading">
+            <div class="bg-blue-500 rounded-lg text-white mb-4">
+              Loading the WASM build of esbuild..
+            </div>
           </div>
           <div class="p-3" v-if="buildError">
             <div
