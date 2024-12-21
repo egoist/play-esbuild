eb51e7e2d09b34741a4439e45315e0cc0440066d
EGOIST <hi@egoist.dev>
2024-12-21 15:01:15 +0800

    tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
-import "./css/tailwind.css"
-import "./css/codemirror.css"
-import { createApp } from "vue"
-import App from "./components/App.vue"
+import "./css/tailwind.css";
+import "./css/codemirror.css";
+import { createApp } from "vue";
+import App from "./components/App.vue";
 
-createApp(App).mount("#app")
+createApp(App).mount("#app");
+
+if (import.meta.env.PROD) {
+  const script = document.createElement("script");
+  script.src = "https://umami2.egoist.dev/script.js";
+  script.defer = true;
+  script.setAttribute(
+    "data-website-id",
+    "fcfe4289-ab86-4d66-91b1-23eb307c25b1"
+  );
+  script.setAttribute("data-auto-track", "false");
+
+  script.onload = () => {
+    if (typeof umami === "undefined") return;
+
+    umami.track((props) => ({
+      ...props,
+      url: location.pathname + location.search,
+    }));
+  };
+
+  document.head.appendChild(script);
+}
